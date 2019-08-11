fc3c459f778da953cf1b1a0049baa2260b9e197f
Vasconcelos <j.a.vasconcelos@accenture.com>
2019-08-11 20:49:47 -0300

    create a first endpoint - getUsers

---
+++ src/main/java/com/example/demo/resource/UserResource.java
@@ -0,0 +1,27 @@
+package com.example.demo.resource;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.entity.User;
+
+@RestController
+@RequestMapping("/api")
+public class UserResource {
+	
+	@GetMapping("/users")
+	public ResponseEntity<List<User>> findAll(){
+		
+		List<User> users = new ArrayList<User>();
+		users.add(new User("João","Pedro","joaopedro@gmail.com"));
+		users.add(new User("Carla","Maria","carla@gmail.com"));
+		
+		return ResponseEntity.ok().body(users);
+		
+	}
+}
